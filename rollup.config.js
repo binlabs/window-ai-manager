3884cb214b7cd69e78360401d52330279991011b
Kurt Maine <develohost@gmail.com>
2024-06-02 17:24:32 -0500

    ci(package): dist both esm and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,23 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
+import terser from '@rollup/plugin-terser'
 
 export default {
     input: 'src/index.ts',
-    output: {
-        file: 'dist/window-ai-manager.js',
-        format: 'cjs',
-        sourcemap: true,
-    },
+    output: [
+        {
+            file: 'dist/window-ai-manager.cjs.js',
+            format: 'cjs',
+            exports: 'auto',
+            sourcemap: true,
+        },
+        {
+            file: 'dist/window-ai-manager.esm.js',
+            format: 'esm',
+            sourcemap: true,
+        },
+    ],
     plugins: [
         resolve({
             extensions: ['.js', '.ts'],
@@ -18,5 +27,6 @@ export default {
         typescript({
             tsconfig: 'tsconfig.json',
         }),
+        terser(),
     ],
 }
